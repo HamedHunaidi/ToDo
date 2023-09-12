d66702b3dd0bfac5bfdcd7766666546be4d69d3c
Hamed Hunaidi <143928918+HamedHunaidi@users.noreply.github.com>
2023-09-12 11:03:33 +0300

    Changed lay out to CustomTkinter, added window, pop up, and buttons

--- To Do app.py
+++ To Do app.py
@@ -1,19 +1,44 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QFont
+import customtkinter
 
 
-def main():
-    app = QApplication([])
-    window = QWidget()
-
-    label = QLabel(window)
-    label.setText("KawaBanga")
-    label.setFont(QFont("Arial", 16))
-    window.setGeometry(100, 100, 200, 300)
-
-    window.show()
-    app.exec_()
+root = customtkinter.CTk()
+root.title("To Do")
+root.geometry("400x400")
+root.resizable(False, False)
 
 
-if __name__ == "__main__":
-    main()
+def popup():
+    global pop
+    global entry1
+    global button2
+
+    pop = customtkinter.CTkToplevel(root)
+    pop.attributes("-topmost", True)
+    pop.title("Enter New Task")
+    pop.geometry("250x150")
+    entry1 = customtkinter.CTkEntry(master=pop, placeholder_text="meow")
+    entry1.pack(pady=12, padx=10)
+    button2 = customtkinter.CTkButton(master=pop, text=" Add Task!", command=addTask)
+    button2.pack(pady=12, padx=10)
+
+
+def addTask():
+    global checkbox1
+    checkbox1 = customtkinter.CTkCheckBox(frame2, text=" Wash yo car")
+    checkbox1.pack()
+
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+label = customtkinter.CTkLabel(master=frame, text="Today's To Do list!")
+label.pack(pady=12, padx=10)
+
+frame2 = customtkinter.CTkFrame(master=frame)
+frame2.pack(pady=12, padx=10)
+
+
+button1 = customtkinter.CTkButton(master=frame, text=" Add a new task!", command=popup)
+button1.pack(pady=12, padx=10)
+
+
+root.mainloop()
