b939dae4ddab9d97cb947c24c8e1326552566bab
Hamed Hunaidi <143928918+HamedHunaidi@users.noreply.github.com>
2023-09-13 11:37:13 +0300

    added delete button, add new tasks is now funcitonal, task lists are created through a txt file

--- To Do app.py
+++ To Do app.py
@@ -1,10 +1,28 @@
 import customtkinter
-
+from CTkListbox import *
+from customtkinter import END
 
 root = customtkinter.CTk()
 root.title("To Do")
-root.geometry("400x400")
+root.geometry("400x500")
 root.resizable(False, False)
+
+task_list = []
+
+
+def openTaskFile():
+    try:
+        global task_list
+        with open("TaskList.txt", "r") as taskfile:
+            tasks = taskfile.readlines()
+        for task in tasks:
+            if task != "\n":
+                task_list.append(task)
+                listbox.insert(END, task)
+
+    except:
+        file = open("TaskList.txt", "w")
+        file.close()
 
 
 def popup():
@@ -23,9 +41,14 @@ def popup():
 
 
 def addTask():
-    global checkbox1
-    checkbox1 = customtkinter.CTkCheckBox(frame2, text=" Wash yo car")
-    checkbox1.pack()
+    task = entry1.get()
+    entry1.delete(0, END)
+
+    if task:
+        with open("TaskList.txt", "a") as taskfile:
+            taskfile.write(f"\n{task}")
+        task_list.append(task)
+        listbox.insert(END, task)
 
 
 frame = customtkinter.CTkFrame(master=root)
@@ -35,10 +58,13 @@ label.pack(pady=12, padx=10)
 
 frame2 = customtkinter.CTkFrame(master=frame)
 frame2.pack(pady=12, padx=10)
+listbox = CTkListbox(frame2)
+listbox.pack()
 
-
+button0 = customtkinter.CTkButton(master=frame, text="Delete Task")
+button0.pack(pady=12, padx=10)
 button1 = customtkinter.CTkButton(master=frame, text=" Add a new task!", command=popup)
 button1.pack(pady=12, padx=10)
 
-
+openTaskFile()
 root.mainloop()
